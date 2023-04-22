eac90ab6fb07cc7375cfefdfd92f3172a661d759
Kavitha316S <316kavithadhana@gmail.com>
2023-04-22 23:41:17 +0530

    payment

--- shipping.sh
+++ shipping.sh
@@ -1,11 +1,11 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
-mysql_root_password =$1
+mysql_root_password=$1
 
 if [ -z "$mysql_root_password" ]; then
-    echo Input MySQL Root Password Missing
-    exit
+  echo Input MySQL Root Password Missing
+  exit
 fi
 
 component="shipping"
