3e3d809c3ba04d9e4f37b06493d01cc1a6e790f2
Kavitha316S <316kavithadhana@gmail.com>
2023-04-22 22:30:52 +0530

    payment

--- common.sh
+++ common.sh
@@ -17,6 +17,37 @@ func_schema_setup() {
    func_print_head "load schema"
   mongo --host mongosh-dev.devopsb62.online </app/schema/${component}.js
  fi
+ if [ "${schema_setup}" == "mysql" ]; then
+      func_print_head  " install mysql client "
+      yum install mysql -y
+      func_print_head  " load schema "
+      mysql -h mysqlsh-dev.devopsb62.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql
+ fi
+}
+
+func_app_prereq() {
+     func_print_head  " create application user "
+     useradd $(app_user)
+
+     func_print_head  " create application directory"
+     rm -rf /app
+     mkdir /app
+
+     func_print_head  " download application content "
+     curl -L -o /tmp/$(component).zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
+
+     func_print_head  " extract application content "
+     cd /app
+     unzip /tmp/$(component).zip
+}
+func_systemd_setup() {
+      func_print_head  " setup systemD service "
+      cp $(script_path)/$(component).service /etc/systemd/system/$(component).service
+
+      func_print_head  " start $(component) service "
+      systemctl daemon-reload
+      systemctl enable $(component)
+      systemctl restart $(component)
 }
 
 func_nodejs() {
@@ -26,71 +57,25 @@ func_nodejs() {
   func_print_head "install nodejs"
   yum install nodejs -y
 
-  func_print_head "add application user"
-  useradd $(app_user)
-
-  func_print_head "create application directory"
-  rm -rf /app
-  mkdir /app
-
-  func_print_head "download app content"
-  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/{component}.zip
-  cd /app
-
-  func_print_head "unzip"
-  unzip /tmp/${component}.zip
+  func_app_prereq
 
   func_print_head "install nodejs dependencies"
   npm install
 
-  func_print_head "copy user systemd file"
-  cp $(script_path)/${component}.service /etc/systemd/system/{component}.service
-
-  func_print_head "start cart service"
-  systemctl daemon-reload
-  systemctl enable ${component}
-  systemctl restart ${component}
-
   func_schema_setup
+  func_sysytemd_setup
  }
 
  func_java() {
-   echo -e "\e[36m>>>>>>>> install maven <<<<<<<<<\e[0m"
+    func_print_head " install maven "
     yum install maven -y
 
-    echo -e "\e[36m>>>>>>>> create app user <<<<<<<<<\e[0m"
-    useradd roboshop
+    func_app_prereq
 
-    echo -e "\e[36m>>>>>>>> create app directory<<<<<<<<<\e[0m"
-    rm -rf /app
-    mkdir /app
-
-    echo -e "\e[36m>>>>>>>> download app content <<<<<<<<<\e[0m"
-    curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
-
-    script_path=$(dirname "$script")
-    source ${script_path}/common.sh
-
-    echo -e "\e[36m>>>>>>>> extract app content <<<<<<<<<\e[0m"
-    cd /app
-    unzip /tmp/shipping.zip
-
-
-    echo -e "\e[36m>>>>>>>> download maven dependencies <<<<<<<<<\e[0m"
+    func_print_head  " download maven dependencies "
     mvn clean package
-    mv target/shipping-1.0.jar shipping.jar
+    mv target/$(component)-1.0.jar (component).jar
 
-     echo -e "\e[36m>>>>>>>> install mysql <<<<<<<<<\e[0m"
-    yum install mysql -y
-
-    echo -e "\e[36m>>>>>>>> load schema <<<<<<<<<\e[0m"
-    mysql -h mysqlsh-dev.devopsb62.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql
-
-    echo -e "\e[36m>>>>>>>> setup systemdservice <<<<<<<<<\e[0m"
-    cp $(script_path)/shipping.service /etc/systemd/system/shipping.service
-
-    echo -e "\e[36m>>>>>>>> start shipping service <<<<<<<<<\e[0m"
-    systemctl daemon-reload
-    systemctl enable shipping
-    systemctl restart shipping
+    func_schema_setup
+    func_systemd_setup
  }
--- shipping.sh
+++ shipping.sh
@@ -3,7 +3,11 @@ script_path=$(dirname "$script")
 source ${script_path}/common.sh
 mysql_root_password =$1
 
-if [ -z "mysql_root_password" ]; then
+if [  -z "$mysql_root_password" ]; then
   echo Input Mysql root  Password Missing
   exit
 fi
+
+component="shipping"
+schema_setup=mysql
+func_java()
