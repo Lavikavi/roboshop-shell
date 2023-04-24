fd0c5399060860679efc5e38e570576234095e3b
Kavitha316S <316kavithadhana@gmail.com>
2023-04-24 19:53:58 +0530

    cart

--- cart.sh
+++ cart.sh
@@ -4,5 +4,17 @@ script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
 component=cart
-func_systemd_setup
 func_nodejs
+
+func_systemd_setup() {
+	  func_print_head "Setup SystemD Service"
+	  cp ${script_path}/${component}.service /etc/systemd/system/${component}.service &>>$log_file
+	  func_stat_check $?
+
+
+	  func_print_head "Start ${component} Service"
+	  systemctl daemon-reload &>>$log_file
+	  systemctl enable ${component} &>>$log_file
+	  systemctl restart ${component} &>>$log_file
+	  func_stat_check $?
+	}
